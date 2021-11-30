7823b0615b463655324cc9b6cb6b6f8c0faafa2c
Danielle Cristina <dnaielle.trybe@gmail.com>
2021-11-30 17:30:07 -0300

    Corrigindo erros, incluindo requisitos 6, 7, 8 e 9

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-// const pixelBoard = document.getElementById('pixel-board');
+const pixelBoard = document.getElementById('pixel-board');
 
-// const row = 5;
-// const columns = 5;
+const row = 5;
+const columns = 5;
 
-// for (let index = 0; index < row; index += 1) {
-//   const pixelLinha = document.createElement('div');
-//   for (let i = 0; i < columns; i += 1) {
-//     const box = document.createElement('div');
-//     box.className = 'pixel';
-//     pixelLinha.appendChild(box);
-//   }
-//   pixelBoard.appendChild(pixelLinha);
-// }
+for (let index = 1; index <= row; index += 1) {
+  const pixelLinha = document.createElement('div');
+  for (let i = 0; i < columns; i += 1) {
+    const box = document.createElement('div');
+    box.className = 'pixel';
+    pixelLinha.appendChild(box);
+  }
+  pixelBoard.appendChild(pixelLinha);
+}
 
-//const inicialCor = document.getElementById('color-one');
-//inicialCor.classList.add('selected');
-
+//define cor inicial
 const oneColor = document.getElementById('color-one');
 oneColor.classList.add('selected');
 
@@ -23,16 +21,39 @@ const twoColor = document.getElementById('color-two');
 const threeColor = document.getElementById('color-three');
 const fourColor = document.getElementById('color-four');
 
-function addColor(event) {
+//Adiciona e remove Classe selected nas cores selecionadas
+function addClass(event) {
   const colorElement = document.querySelector('.selected');
   colorElement.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
-oneColor.addEventListener('click', addColor);
-twoColor.addEventListener('click', addColor);
-threeColor.addEventListener('click', addColor);
-fourColor.addEventListener('click', addColor);
+oneColor.addEventListener('click', addClass);
+twoColor.addEventListener('click', addClass);
+threeColor.addEventListener('click', addClass);
+fourColor.addEventListener('click', addClass);
 
-// Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada
-const selectedColor = document.querySelector('.selected');
+let corSelect = document.querySelector('.selected').style.background;//guarda a cor selecionada
+
+//passar por quadro de pixels
+const pixels = document.querySelectorAll('.pixel');
+
+for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', preencheBox());
+}
+
+// Clicar na cor escolhida e preencher quadro
+function preencheBox(corSelect) {
+  pixels.style.background = corSelect;
+}
+
+//Limpar Pixels
+let btnLimpar = document.getElementById('clear-board');
+btnLimpar.addEventListener('click', clear);
+
+function clear() {
+  for (let index = 0; index <= 25; index+=1) {
+    let pixelClear = document.querySelectorAll('.pixel')[index];
+    pixelClear.style.background = "white";
+  }
+}
