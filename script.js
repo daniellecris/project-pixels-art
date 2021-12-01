a18d151f5e579c309054f86c94e6eb245d60cdfa
Danielle Cristina <dnaielle.trybe@gmail.com>
2021-11-30 21:52:08 -0300

    Corrigindo Erros

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const pixelBoard = document.getElementById('pixel-board');
+const colorPalette = document.getElementById('color-palette');
+colorPalette.addEventListener('click', addClass);
 
 const row = 5;
 const columns = 5;
@@ -13,47 +15,39 @@ for (let index = 1; index <= row; index += 1) {
   pixelBoard.appendChild(pixelLinha);
 }
 
-//define cor inicial
-const oneColor = document.getElementById('color-one');
-oneColor.classList.add('selected');
-
-const twoColor = document.getElementById('color-two');
-const threeColor = document.getElementById('color-three');
-const fourColor = document.getElementById('color-four');
-
 //Adiciona e remove Classe selected nas cores selecionadas
 function addClass(event) {
   const colorElement = document.querySelector('.selected');
+  //Verifica se o elemento selecionado é igual ao Pai, sendo verdadeiro para a função!
+  //Explicação do Tonn
+  if (event.target == colorPalette) return; 
+
   colorElement.classList.remove('selected');
-  event.target.classList.add('selected');
+  event.target.classList.add('selected'); 
 }
 
-oneColor.addEventListener('click', addClass);
-twoColor.addEventListener('click', addClass);
-threeColor.addEventListener('click', addClass);
-fourColor.addEventListener('click', addClass);
-
-let corSelect = document.querySelector('.selected').style.background;//guarda a cor selecionada
+let corSelect = document.querySelector('.selected').style.backgroundColor;//guarda a cor selecionada
 
 const pixels = document.querySelectorAll('.pixel');
+console.log(corSelect)
 
 // Clicar na cor escolhida e preencher quadro
 function preencheBox(corSelect) {
-  pixels[index].style.background = corSelect;
+   pixels[index].style.background = corSelect;
 }
 
-//passar por quadro de pixels
-for (let index = 0; index < pixels.length; index += 1) {
-  pixels[index].addEventListener('click', preencheBox(corSelect));
-}
+// //passar por quadro de pixels
+// for (let index = 0; index < pixels.length; index += 1) {
+//   pixels[index].addEventListener('click', preencheBox(corSelect));
+// }
 
-//Limpar Pixels
-let btnLimpar = document.getElementById('clear-board');
-btnLimpar.addEventListener('click', clear);
+// //Limpar Pixels
+// let btnLimpar = document.getElementById('clear-board');
+// btnLimpar.addEventListener('click', clear);
 
-function clear() {
-  for (let index = 0; index <= pixels.length; index+=1) {
-    //let pixelClear = document.querySelectorAll('.pixel')[index];
-    pixels[index].style.background = 'white';
-  }
-}
+// function clear() {
+//   for (let index = 0; index <= pixels.length; index+=1) {
+//     //let pixelClear = document.querySelectorAll('.pixel')[index];
+//     pixels[index].style.background = 'white';
+//   }
+// }
